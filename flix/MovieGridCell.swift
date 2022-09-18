35be1d402502381231ef7532d758a3293389cf1e
Efren Ramirez <ramirefr2000@gmail.com>
2022-09-18 12:43:25 -0700

    "Flix part 2"

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Efren Ramirez on 9/16/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
